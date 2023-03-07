8cc58ab1c7626c9cc9cf8675b8e9aa7a671f95e9
efdalsahin <muhammetefdalsahin@gmail.com>
2023-03-07 22:56:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("sum-el");
+
+async function startGame() {
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? ";
+    } else if (sum === 21) {
+        message = "You've got Blackjack! ";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! ";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
+}
