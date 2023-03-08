88e975b51472d0f2ad8816e3ae8fb6a3dc4866af
efdalsahin <muhammetefdalsahin@gmail.com>
2023-03-08 23:57:54 +0300

    adding random card feature

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-let firstCard = 10;
-let secondCard = 4;
-
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomCard() {
+    let random = Math.floor(Math.random() * 13) + 1;
+    if (random == 1) {
+        return 11;
+    } else if (random > 10) {
+        return 10;
+    } else {
+        return random;
+    }
+}
+
 function startGame() {
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards.push(firstCard);
+    cards.push(secondCard);
+    sum = firstCard + secondCard;
     renderGame(0);
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    cardsEl.textContent = "Cards: ";
 
+    for (let index = 0; index < cards.length; index++) {
+        cardsEl.textContent += " " + cards[index];
+    }
     sumEl.textContent = "Sum: " + sum;
 
     if (sum <= 20) {
@@ -35,7 +51,7 @@ function renderGame() {
 
 function newCard() {
     console.log("Drawing a new card from the deck!");
-    let card = 7;
+    let card = getRandomCard();
     sum += card;
 
     cards.push(card);
